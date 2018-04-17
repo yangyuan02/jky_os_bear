11dfdf870e87762aecb52c633348cf915f8de815
yangyuan <1367511704@qq.com>
2018-04-17 13:54:45 +0800

    全局引入ele-ui组件

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # 教科院 - 后台管理
-# 开发环境
-cp config/index.js.example config/index.js
-docker network create thrive_jky_bear
-docker-compose build
-docker-compose run app yarn install
-docker-compose up -d
+### 开发环境
+
+	cp config/index.js.example config/index.js
+
+	docker network create thrive_jky_bear
+
+	docker-compose build
+	docker-compose run app yarn install
+	docker-compose up -d
 
 访问浏览器地址 http://127.0.0.1:8080
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,142 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>Try Element</p>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: function() {
+        return { visible: false }
+      }
 }
 </script>
 
 <style>
+.clear:after{display:block;clear:both;content:"";visibility:hidden;height:0}
+.clear{zoom:1}
+html,body,#app{
+  width: 100%;
+  height: 100%;
+  -webkit-tap-highlight-color:rgba(0,0,0,0)
+}
+.move-enter-active,.move-leave-active{
+    transition: opacity .5s;
+}
+.move-enter,.move-leave{
+    opacity: 0;
+}
+div,input{
+    box-sizing: border-box;
+}
+body{
+    font-family: "仿宋" !important;
+    font-size:14px;
+}
+body,
+dd,
+dl,
+dt,
+fieldset,
+form,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+html,
+img,
+legend,
+li,
+ol,
+p,
+ul {
+    margin: 0;
+    padding: 0;
+    -moz-user-select:none;-webkit-user-select:none;
+    -webkit-tap-highlight-color:transparent;
+}
+button,
+fieldset,
+img,
+input {
+    border: none;
+    padding: 0;
+    margin: 0;
+    outline-style: none;
+}
+ul,li {
+  /* background-color: #ffffff; */
+}
+ol,
+ul {
+    list-style: none;
+}
+input {
+    padding-top: 0;
+    padding-bottom: 0;
+    font-family: "SimSun", "宋体";
+}
+input,
+select {
+    vertical-align: middle;
+}
+input,
+select,
+textarea {
+    font-size: 12px;
+    margin: 0;
+}
+input,
+    button,
+    select,
+    textarea {
+        outline: none;
+    }
+textarea {
+    resize: none;
+}
+/*防止拖动*/
+img {
+    border: 0;
+    vertical-align: middle;
+}
+/*  去掉图片低测默认的3像素空白缝隙*/
+table {
+    border-collapse: collapse;
+}
+/* 去斜体 */
+i {
+  font-style:normal
+}
+body {
+    font-family: PingFangSC-Light,helvetica,'Heiti SC';
+    color: #666;
+    background: #fff;
+}
+a {
+    color: #666;
+    text-decoration: none;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    text-decoration: none;
+    font-weight: normal;
+    font-size: 100%;
+    font-family: PingFangSC-Light,helvetica,'Heiti SC';
+}
+.ac {
+    text-align: center;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
