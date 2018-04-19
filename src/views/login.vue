4188aea59193f1b1a94e76918f36ccec723aaebd
yangyuan <1367511704@qq.com>
2018-04-20 00:10:00 +0800

    修改编码tap空格/node代理接口/生成验证码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,79 +1,89 @@
 <template>
-  <div class="login-wrap">
-    <div class="login_box">
-      <div class="ms-title">后台管理系统</div>
-      <div class="login_content">
-        <div class="ms-login">
-          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
-            <el-form-item prop="username">
-              <el-input v-model="ruleForm.username" placeholder="username"></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
-            </el-form-item>
-            <div class="login-btn">
-              <el-button type="primary">登录22aaa</el-button>
+    <div class="login-wrap">
+        <div class="login_box">
+            <div class="ms-title">后台管理系统</div>
+            <div class="login_content">
+                <div class="ms-login">
+                    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
+                        <el-form-item prop="username">
+                            <el-input v-model="ruleForm.username" placeholder="username"></el-input>
+                        </el-form-item>
+                        <el-form-item prop="password">
+                            <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+                        </el-form-item>
+                        <div class="login-btn">
+                            <el-button type="primary">登录22aaa</el-button>
+                        </div>
+                    </el-form>
+                </div>
             </div>
-          </el-form>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        ruleForm: {
-          username: '',
-          password: ''
+    export default {
+        data() {
+            return {
+                ruleForm: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    username: [{
+                        required: true,
+                        message: '请输入用户名',
+                        trigger: 'blur'
+                    }],
+                    password: [{
+                        required: true,
+                        message: '请输入密码',
+                        trigger: 'blur'
+                    }]
+                }
+            }
         },
-        rules: {
-          username: [{
-            required: true,
-            message: '请输入用户名',
-            trigger: 'blur'
-          }],
-          password: [{
-            required: true,
-            message: '请输入密码',
-            trigger: 'blur'
-          }]
+        methods: {
+            creatCode() { //生成验证码
+                this.$ajax.get("/api/captcha/show", {}).then((res) => {
+                    console.log(res)
+                }, (err) => {})
+            }
+        },
+        mounted(){
+            this.creatCode()
         }
-      }
     }
-  }
 </script>
 
 <style scoped lang="less">
-  .login-wrap {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background: #324157;
-    .login_box{
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      text-align: center;
-      .login_content{
-        height: 240px;
-        width: 340px;
-        background: #fff;
-        padding: 40px;
-        border-radius: 4px;
-        margin-top: 30px;
-        .login-btn button{
-          width: 100%;
-          height: 36px;
+    .login-wrap {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        background: #324157;
+        .login_box {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            .login_content {
+                height: 240px;
+                width: 340px;
+                background: #fff;
+                padding: 40px;
+                border-radius: 4px;
+                margin-top: 30px;
+                .login-btn button {
+                    width: 100%;
+                    height: 36px;
+                }
+            }
         }
-      }
+        .ms-title {
+            font-size: 30px;
+            color: #fff;
+        }
     }
-    .ms-title{
-      font-size: 30px;
-      color: #fff;
-    }
-  }
 </style>
